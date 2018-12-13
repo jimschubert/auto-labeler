bcde3f30dcc52ed4243fda3ba08f3ab01de70e57
Jim Schubert <james.schubert@gmail.com>
2018-12-13 09:07:25 -0500

    Include command line utils for init/validate of config files

--- unit/model/config.spec.js
+++ unit/model/config.spec.js
@@ -3,11 +3,16 @@ const fs = require('fs')
 const path = require('path')
 const yaml = require('js-yaml')
 const config = require('../../lib/model/config.js').config
+const Types = require('../../lib/model/config.js').Types
+
+let fixture = (file) => {
+  return path.normalize(path.join(__dirname, '..', 'fixtures', file))
+}
 
 describe('Config', () => {
   describe('#config(obj)', () => {
     it('should parse a "simple" config object', (done) => {
-      let p = path.normalize(path.join(__dirname, '..', 'fixtures', 'simple.yml'))
+      let p = fixture('simple.yml')
 
       let simple = yaml.safeLoad(fs.readFileSync(p))
       let c = config(simple)
@@ -35,7 +40,7 @@ describe('Config', () => {
     })
 
     it('should parse a "full" config object', () => {
-      let p = path.normalize(path.join(__dirname, '..', 'fixtures', 'full.yml'))
+      let p = fixture('full.yml')
 
       let full = yaml.safeLoad(fs.readFileSync(p))
       let c = config(full)
@@ -60,6 +65,18 @@ describe('Config', () => {
       assert.equal(enhancement.include.length, 1)
       assert.equal(enhancement.exclude.length, 0)
       assert.equal(String(enhancement.include[0]), String(/\bfeat\b/g))
+    })
+
+    it('should expose a "full" schema error object via "throws=true" option', () => {
+      let p = fixture('full.bad.yml')
+      let full = yaml.safeLoad(fs.readFileSync(p))
+      assert.throws(() => {
+        config(full, { throws: true })
+      }, function (err) {
+        if ((err instanceof Types.ConfigError) && err.message === 'Invalid Config') {
+          return true
+        }
+      })
     })
   })
 })
